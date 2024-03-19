692c76a54f7beb081cdbdaac3b93ed17c11154da
nmajuterence <nmajuterence@gmail.com>
2024-03-19 02:14:55 +0100

    0. Print a list of integers update1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def print_list_integer(my_list=None):
+    if my_list is None:
+        my_list = []
     for item in my_list:
         if isinstance(item, int):
-            print(item)
+            print("{}".format(item))
