b029b0cfbaaf5f38a59a7a0f5088335d1ca09973
nmajuterence <nmajuterence@gmail.com>
2024-03-19 03:53:36 +0100

    7. Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Extract elements from the tuples, using 0 if a tuple is smaller than 2
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    # Perform addition for each element and return the result as a tuple
+    return (a[0] + b[0], a[1] + b[1])
