cf9e51534118e717191ccd9969315e0b0b442c45
nmajuterence <nmajuterence@gmail.com>
2024-03-19 02:41:03 +0100

    1. Secure access to an element in a list update1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
+    # Check for invalid index conditions:
     if idx < 0 or idx >= len(my_list):
         return None
-    else:
-        return my_list[idx]
 
+    # Index is valid, return the element:
+    return my_list[idx]
