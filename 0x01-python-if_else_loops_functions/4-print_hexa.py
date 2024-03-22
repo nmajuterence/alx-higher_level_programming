e9a5cc1e50bcdf4e199a0d748d3fa923bb214174
nmajuterence <nmajuterence@gmail.com>
2024-03-22 02:34:45 +0100

    4. Hexadecimal printing

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{:d} 0x{:02X}".format(i, i), end='\n' if i % 10 == 9 else ' ')
+for x in range(0, 99):
+    print("{0} = {1}".format(x, hex(x)))
+
