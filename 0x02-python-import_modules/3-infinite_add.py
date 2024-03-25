2eb5e4d129959b6be2b5bafb042a2cdbfa4d8d90
nmajuterence <nmajuterence@gmail.com>
2024-03-25 15:24:10 +0100

    remove files

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-if __name__ == "__main__":
-    # the program add an infinite len of integers
-    from sys import argv
-
-    sum = 0
-    for x in range(1, len(argv)):
-        sum += int(argv[x])
-    print("{}".format(sum))
