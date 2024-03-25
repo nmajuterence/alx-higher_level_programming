2e6d2cadbb08f3683d13af50708e3163d9f1df26
nmajuterence <nmajuterence@gmail.com>
2024-03-25 13:49:37 +0100

    3. Infinite addition update1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     # the program add an infinite len of integers
     import sys
 
-    totalAddition = 0
+    total = 0
     for x in range(len(sys.argv) - 1):
-        totalAddition += int(sys.argv[x + 1])
-    print("{}".format(totalAddition))
+        total += int(sys.argv[x + 1])
+    print("{}".format(total))
