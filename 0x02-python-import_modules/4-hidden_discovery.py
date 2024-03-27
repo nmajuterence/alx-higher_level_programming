35ea3d538a6b9d0f942abf9cc1de6b1882ad819f
nmajuterence <nmajuterence@gmail.com>
2024-03-28 00:57:17 +0100

    program that prints all the names defined by the compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This program prints
+all named functions in
+a compiled file."""
+if __name__ = "__main__":
+    from hidden-4 import *
+
+    namedfs = dir()
+    for x in range(0, len(namedfs)):
+        if namedfs[x][:2] == "__":
+            continue
+        else:
+            print("{:s}".format(namedfs))
