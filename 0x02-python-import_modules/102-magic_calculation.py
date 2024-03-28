e85b6c692592f19726d12cf761f81e3ad467bdb5
nmajuterence <nmajuterence@gmail.com>
2024-03-28 13:21:05 +0100

    program that prints the alphabet in uppercase

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""python magic calculator"""
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-
-    if a < b:
+if __name__ == "__main__":
+    from magic_calculation import add, sub
+    def magic_calculation(a, b):
+        if a < b:
         c = add(a, b)
         for x in range(4, 6):
             c = add(c, x)
-        return (c)
-
-    else:
-        return(sub(a, b))
+            return (c)
+        else:
+            return (sub(a, b))
