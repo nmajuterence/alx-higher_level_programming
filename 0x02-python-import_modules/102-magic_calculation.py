f85722956b3b598cf4686ff9711afe2ffc10ea41
nmajuterence <nmajuterence@gmail.com>
2024-03-28 11:39:47 +0100

    program that prints the python magic calculator upudate

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""magic calculator in python"""
-if __name__ == "__main__":
+"""python magic calculator"""
+def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
-    def magic_calculation(a, b):
-        if a < b:
-            c = sub(a, b)
-            return (c)
-        else:
-            for i in range(4, 6):
-                c = add(c, i)
-            return (c)
+    if a < b:
+        c = add(a, b)
+        for x in range(4, 6):
+            c = add(c, x)
+        return (c)
+
+    else:
+        return(sub(a, b))
