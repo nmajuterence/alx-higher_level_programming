41b821c5d6fc91a7d350e5fc0c1c1c94f4d91618
nmajuterence <nmajuterence@gmail.com>
2024-03-28 02:40:41 +0100

    program that imports all functions from the file calculator_1.py and handles basic operations.

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This program that imports
+all functions from the file
+calculator_1.py and handles
+basic operations."""
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    argCount = len(argv)
+    if argCount != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+
+    if operator == "+":
+        print(f"{a} {operator} {b} = {add(a, b)}")
+    elif operator == "-":
+        print("{a} {operator} {b} = {sub(a, b)}")
+    elif operator == "*":
+        print("{a} {operator} {b} = {mul(a, b)}")
+    elif operator == "/":
+        print("{a} {operator} {b} = {div(a, b)}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
