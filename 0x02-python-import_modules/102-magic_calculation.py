7cfebc1c27d596971e3d83ad4e4869fb5c33a037
nmajuterence <nmajuterence@gmail.com>
2024-03-28 13:41:44 +0100

    Python bytecode update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""python magic calculator"""
 if __name__ == "__main__":
-    from magic_calculation import add, sub
-
     def magic_calculation(a, b):
+        from magic_calculation_102 import add, sub
         if a < b:
             c = add(a, b)
-        for x in range(4, 6):
-            c = add(c, x)
-            return (c)
+            for i in range(4, 6):
+                c = add(c, i)
+            return c
         else:
-            return (sub(a, b))
+            return sub(a, b)
