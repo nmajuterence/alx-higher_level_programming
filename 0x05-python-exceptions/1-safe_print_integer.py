323f2690ef6f18c98283114a632337df5a2cc53a
nmajuterence <nmajuterence@gmail.com>
2024-04-03 00:56:26 +0100

    Write a function that prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""function that prints an
+integer with "{:d}".format()."""
+
+
+def safe_print_integer(value):
+    try:
+            value = int(value)
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+            print(value)
+            return False
