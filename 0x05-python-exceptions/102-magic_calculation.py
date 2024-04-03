0bf342e0880eab3209dfa347adae170f161e4e57
nmajuterence <nmajuterence@gmail.com>
2024-04-03 09:39:36 +0100

    analyze the bytecode and replicate its behavior in Python code.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,11 +5,11 @@ replicate its behavior in Python code."""
 
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):
+    for x in range(1, 3):
         try:
-            if i > a:
-                raise Exception('Too far')
-            result += (a ** b) / i
+            if x > a:
+                raise Exception("Too far")
+            result += (a ** b) / x
         except:
             result += a + b
             break
