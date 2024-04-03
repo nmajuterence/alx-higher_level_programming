163e5cb0725a8d98f39f5b4fb1eb54acae443098
nmajuterence <nmajuterence@gmail.com>
2024-04-03 01:27:16 +0100

    Write a function that prints an integer update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,6 @@ integer with "{:d}".format()."""
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return (False)
+        return True
+    except ValueError:
+        return False
